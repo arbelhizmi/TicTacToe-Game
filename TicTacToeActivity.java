d652ba338280a33ed493668cb5d505c09a789dfa
arbelhizmi <43061885+arbelhizmi@users.noreply.github.com>
2019-04-25 12:16:51 +0300

    tictactoe-game

---
+++ TicTacToeActivity.java
@@ -0,0 +1,98 @@
+package com.example.galel.tictactoegame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+public class TicTacToeActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button Square00; private Button Square01; private Button Square02;
+    private Button Square10; private Button Square11; private Button Square12;
+    private Button Square20; private Button Square21; private Button Square22;
+    private TicTacToeGame gameManager;
+    private Button NewGame;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tic_tac_toe);
+        Square00 = (Button) findViewById(R.id.Square00);
+        Square01 = (Button) findViewById(R.id.Square01);
+        Square02 = (Button) findViewById(R.id.Square02);
+        Square10 = (Button) findViewById(R.id.Square10);
+        Square11 = (Button) findViewById(R.id.Square11);
+        Square12 = (Button) findViewById(R.id.Square12);
+        Square20 = (Button) findViewById(R.id.Square20);
+        Square21 = (Button) findViewById(R.id.Square21);
+        Square22 = (Button) findViewById(R.id.Square22);
+        NewGame = (Button) findViewById(R.id.NewGame);
+        gameManager = new TicTacToeGame(Square00, Square01, Square02, Square10, Square11, Square12, Square20, Square21, Square22);
+        Square00.setOnClickListener(this);
+        Square01.setOnClickListener(this);
+        Square02.setOnClickListener(this);
+        Square10.setOnClickListener(this);
+        Square11.setOnClickListener(this);
+        Square12.setOnClickListener(this);
+        Square20.setOnClickListener(this);
+        Square21.setOnClickListener(this);
+        Square22.setOnClickListener(this);
+        NewGame.setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.Square00:
+                this.gameManager.allMove(this.Square00);
+                printWinner();
+                break;
+            case R.id.Square01:
+                this.gameManager.allMove(this.Square01);
+                printWinner();
+                break;
+            case R.id.Square02:
+                this.gameManager.allMove(this.Square02);
+                printWinner();
+                break;
+            case R.id.Square10:
+                this.gameManager.allMove(this.Square10);
+                printWinner();
+                break;
+            case R.id.Square11:
+                this.gameManager.allMove(this.Square11);
+                printWinner();
+                break;
+            case R.id.Square12:
+                this.gameManager.allMove(this.Square12);
+                printWinner();
+                break;
+            case R.id.Square20:
+                this.gameManager.allMove(this.Square20);
+                printWinner();
+                break;
+            case R.id.Square21:
+                this.gameManager.allMove(this.Square21);
+                printWinner();
+                break;
+            case R.id.Square22:
+                this.gameManager.allMove(this.Square22);
+                printWinner();
+                break;
+            case R.id.NewGame:
+                gameManager.createNewGame();
+                break;
+        }
+    }
+
+    // a method that print Toast of the winner in the game or print its Draw
+    public void printWinner(){
+        if(!this.gameManager.checkWin(gameManager.ticTacToe).equals("")){
+            if(this.gameManager.checkWin(gameManager.ticTacToe).equals("Draw")){
+                Toast.makeText(this, "No winner, it's a Draw", Toast.LENGTH_LONG).show();
+            }else {
+                Toast.makeText(this, "player " + gameManager.checkWin(gameManager.ticTacToe) + " win", Toast.LENGTH_LONG).show();
+            }
+            gameManager.disable(false);
+        }
+    }
+}
---
+++ TicTacToeGame.java
@@ -0,0 +1,120 @@
+package com.example.galel.tictactoegame;
+// Gal Elefersy 315850008
+import android.widget.Button;
+public class TicTacToeGame {
+    Button[][] ticTacToe;
+    private int player;
+    private int computer;
+
+    // a constructor
+    public TicTacToeGame(Button Square00, Button Square01, Button Square02, Button Square10,Button Square11, Button Square12, Button Square20,Button Square21, Button Square22){
+        ticTacToe = new Button[3][3];
+        player = 0;
+        computer = 0;
+        ticTacToe[0][0] = Square00; ticTacToe[0][1] = Square01; ticTacToe[0][2] = Square02;
+        ticTacToe[1][0] = Square10; ticTacToe[1][1] = Square11; ticTacToe[1][2] = Square12;
+        ticTacToe[2][0] = Square20; ticTacToe[2][1] = Square21; ticTacToe[2][2] = Square22;
+        for(int i=0; i<9; i++){
+            ticTacToe[i/3][i%3].setText("");
+        }
+    }
+
+    // a method that get all the game move
+    public void allMove(Button Square){
+        String str = checkWin(ticTacToe);
+        if(str.equals("")) {
+            playerMove(Square);
+            str = checkWin(ticTacToe);
+            if(str.equals("")) {
+                if (player == computer + 1) {
+                    computerMove();
+                }
+            }
+        }
+    }
+
+    // a method for place X as the player move
+    public void playerMove(Button Square){
+        if(Square.getText().toString().equals("")) {
+            Square.setText("X");
+            player++;
+        }
+    }
+    // a method for place O as the computer move
+    public void computerMove(){
+        if(player < 5) {
+            boolean found = false;
+            while (found == false) {
+                int i = (int) (Math.random() * 3);
+                int j = (int) (Math.random() * 3);
+                if (ticTacToe[i][j].getText().toString().equals("")) {
+                    ticTacToe[i][j].setText("O");
+                    computer++;
+                    found = true;
+                    return;
+                }
+            }
+        }
+    }
+
+    // a method check if the computer or the player has won or if its a draw
+    public String checkWin(Button[][] arr){
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[0][i].getText().toString().equals(arr[1][i].getText().toString())){
+                if (arr[1][i].getText().toString().equals(arr[2][i].getText().toString())){
+                    if (!arr[0][i].getText().toString().equals("")) {
+                        return arr[0][i].getText().toString();
+                    }
+                }
+            }
+            if(arr[i][0].getText().toString().equals(arr[i][1].getText().toString())){
+                if (arr[i][1].getText().toString().equals(arr[i][2].getText().toString())) {
+                    if (!arr[i][0].getText().toString().equals("")) {
+                        return arr[i][0].getText().toString();
+                    }
+                }
+            }
+        }
+        if(arr[0][0].getText().toString().equals(arr[1][1].getText().toString())){
+            if (arr[1][1].getText().toString().equals(arr[2][2].getText().toString())) {
+                if (!arr[0][0].getText().toString().equals("")) {
+                    return arr[0][0].getText().toString();
+                }
+            }
+        }
+        if(arr[2][0].getText().toString().equals(arr[1][1].getText().toString())){
+            if (arr[1][1].getText().toString().equals(arr[0][2].getText().toString())) {
+                if (!arr[2][0].getText().toString().equals("")) {
+                    return arr[2][0].getText().toString();
+                }
+            }
+        }
+        int count = 0;
+        for(int i=0; i<9; i++){
+            if(!arr[i/3][i%3].getText().toString().equals("")){
+                count++;
+            }
+        }
+        if(count == 9){
+            return "Draw";
+        }
+        return "";
+    }
+
+    // a method for reset the board and start new game
+    public void createNewGame(){
+        for(int i=0; i<9; i++){
+            ticTacToe[i/3][i%3].setText("");
+        }
+        player = 0;
+        computer = 0;
+        disable(true);
+    }
+
+    // a method for close the Button if the game has finish
+    public void disable(boolean status){
+        for(int i=0; i<9; i++){
+            ticTacToe[i/3][i%3].setEnabled(status);
+        }
+    }
+}
